7f7b3b70e8d8e4933b96f5935b168899f7c553a5
YSoejima <gureiosugguto@gmail.com>
2021-08-29 14:02:10 +0900

    Titleを作った

---
+++ Assets/Script/Event/Title.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField] Image image;
+
+    public void Titlechange()
+    {
+        StartCoroutine(SceneLoad());
+    }
+
+    IEnumerator SceneLoad()
+    {
+        float a = 0;
+        while (a < 0.9)
+        {
+            a += 0.002f;
+            image.color = new Color(0, 0, 0, a);
+            yield return new WaitForEndOfFrame();
+        }
+        SceneManager.LoadScene("MainScene");
+    }
+}
