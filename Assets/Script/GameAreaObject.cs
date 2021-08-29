4ae092eb0a1ba8e051e0e8bfeb118bf4e2ac5be2
sorude26 <sorude26@gmail.com>
2021-08-29 09:39:32 +0900

    GameArea外に出たら消えるスクリプトを追加した

---
+++ Assets/Script/GameAreaObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAreaObject : MonoBehaviour
+{
+    [Header("範囲外に出たら破壊するタグの名前")]
+    [SerializeField] string m_areaName = "GameArea";
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == m_areaName)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
