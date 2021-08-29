c34d978b247e4448d9a5bf920b68d2a3c77cb68a
YSoejima <gureiosugguto@gmail.com>
2021-08-29 14:07:36 +0900

    ゲーム終了後タイトルへ戻れるように

---
+++ Assets/Script/Event/GameEnd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using UnityEngine.SceneManagement;
+
+
+public class GameEnd : MonoBehaviour
+{
+    [SerializeField] GameManager m_gameManager;
+    [SerializeField] string m_sceneName;
+    private void Awake()
+    {
+        m_gameManager.GameEnd.Subscribe(_ => End()).AddTo(this);
+        this.gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(m_sceneName);
+    }
+
+    void End()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
