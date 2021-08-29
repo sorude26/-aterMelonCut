5f0ed6adb8f0b8542e458cb64a8b9ef4d740f45f
sorude26 <sorude26@gmail.com>
2021-08-29 10:02:30 +0900

    マウスに追従するスクリプト、Prefabを追加した

---
+++ Assets/Script/FollowMouse.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    [SerializeField] GameObject m_cutArea;
+    private Vector3 position;
+    private Vector3 screenToWorldPointPosition;
+    bool m_cutMode;
+    private void Start()
+    {
+        m_cutMode = false;
+        m_cutArea.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (!m_cutMode)
+            {
+                m_cutMode = true;
+                m_cutArea.SetActive(true);
+            }
+            return;
+        }
+        position = Input.mousePosition;
+        position.z = 10f;
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+        gameObject.transform.position = screenToWorldPointPosition;
+        if (m_cutMode)
+        {
+            m_cutMode = false;
+            m_cutArea.SetActive(false);
+        }
+    }
+}
